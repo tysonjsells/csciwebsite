d24b5b0e491462f9b59a921361a5fa623681cb80
Tyson Sells <128880117+tysonsells@users.noreply.github.com>
2023-11-19 21:54:37 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,9 +23,60 @@ export default function Home() {
           climbing, camping, and about every other hobby in the mountains!"
         </p>
       </main>
-
-      {/* ... remaining code ... */}
     </div>
   );
 }
 
+// Move this block outside the component
+<style jsx global>{`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+      Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+      sans-serif;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  main {
+    padding: 5rem 0;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .title {
+    margin: 0;
+    line-height: 1.15;
+    font-size: 3rem;
+  }
+
+  .underline {
+    margin-bottom: 1.5rem;
+  }
+
+  .description {
+    line-height: 1.5;
+    font-size: 1.5rem;
+  }
+
+  .pic {
+    height: 450px;
+    width: 800px;
+  }
+
+  code {
+    background: #fafafa;
+    border-radius: 5px;
+    padding: 0.75rem;
+    font-size: 1.1rem;
+    font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+      DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+  }
+`}</style>
